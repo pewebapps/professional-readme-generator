410a6aa8b4103e8c9ec7a552f770ec9b59727ef7
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-02-26 18:50:07 +0000

    refactor how prompt is asked

--- index.js
+++ index.js
@@ -5,29 +5,27 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter the title of your README:"
+    }
 ];
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "Please enter the title of your README:"
-        }
-    ])
-    .then((answers) => {
-        console.log(answers);
-    })
-    .catch((error) => {
-        console.log("An error occurred: ");
-    })
+
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+    })
+    .catch((error) => {
+        console.log("An error occurred: ");
+    })
 }
 
 // function call to initialize program

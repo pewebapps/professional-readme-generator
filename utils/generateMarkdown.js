3f0c47aafdc5da9740c7cb8d9a86d5bf1ec6a3de
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-02-27 10:02:27 +0000

    parse answers into there relevant sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,17 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
   ## Description
   ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
 `;
 }
 
